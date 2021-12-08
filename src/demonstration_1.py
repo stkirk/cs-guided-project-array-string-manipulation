cc3413d31c9c04b7eccf0f652ad6727b0e581159
stkirk <shaanekirk@gmail.com>
2021-12-08 18:00:17 -0500

    review lecture

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -8,7 +8,7 @@ If the input array does not have a "pivot" index, then the function should retur
 Example 1:
 
 Input: nums = [1,7,3,6,5,6]
-Output: 3
+Output: 3 (value 6)
 Explanation:
 The sum of the numbers to the left of index 3 (1 + 7 + 3 = 11) is equal to the sum of numbers to the right of index 3 (5 + 6 = 11).
 Also, 3 is the first index where this occurs.
@@ -21,5 +21,52 @@ Explanation:
 There is no index that satisfies the conditions in the problem statement.
 """
 def pivot_index(nums):
-    # Your code here
+    # loop through the array
+    for i in range(len(nums)): #--> O(n)
 
+        # is the current item the pivot index?
+        # sum stuff on the left (from 0 to the index)
+        left_sum = sum(nums[:i]) #--> O(n/2)
+
+        # sum stuff on the right (from index + 1 to the end)
+        right_sum = sum(nums[i + 1:]) #--> O(n/2)
+
+        # if sums are equal, return the index
+        if left_sum == right_sum:
+            return i
+    # no match? return -1
+    return -1
+# This solution is O(n^2) quadratic time complexity
+
+print("O(n^2-->", pivot_index([1,7,3,6,5,6])) # 3
+print("O(n^2-->", pivot_index([1,2,3])) # -1
+print("O(n^2-->", pivot_index([1])) # 0
+print("O(n^2-->", pivot_index([0, 1, 0])) # 1
+print("O(n^2-->", pivot_index([])) # -1
+
+#Optimize: If you're going to loop through something, use space and memory to avoid
+# O(n) operations within the loop
+def pivot_index_opt(nums):
+    #initialize the sums
+    left_sum = 0 #--> O(1)
+    right_sum = sum(nums) #--> O(n)
+    
+    # loop through the array
+    for i in range(len(nums)): #--> O(n)
+        # remove current index value from right_sum
+        right_sum = right_sum - nums[i] #--> O(1)
+        # check sums and return if they match
+        if left_sum == right_sum: #--> O(1)
+            return i
+        # add current index value to left_sum
+        # doing this after the check that way we already have the left_sum for the next index to check against right_sum the next time through
+        left_sum = left_sum + nums[i] #--> O(1)
+    # no match? return -1
+    return -1
+# This solution is O(n) linear time complexity
+
+print("Optimized-->", pivot_index_opt([1,7,3,6,5,6])) # 3
+print("Optimized-->", pivot_index_opt([1,2,3])) # -1
+print("Optimized-->", pivot_index_opt([1])) # 0
+print("Optimized-->", pivot_index_opt([0, 1, 0])) # 1
+print("Optimized-->", pivot_index_opt([])) # -1
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -26,5 +26,31 @@ Output: [1,0,0,0]
 Explanation: The input array represents the integer 999. 999 + 1 = 1000.
 """
 def plus_one(digits):
-    # Your code here
+    # make a copy of digits
+    digits = digits[:] #--> O(n)
+    # loop from the end of the array
+    # range(start at the end of the array, go to the upper bound of -1, get there by moving -1 index at a time)
+    for i in range(len(digits) - 1, -1, -1): #--> O(n)
+        # add one to the current digit
+        digits[i] += 1
+        if digits[i] < 10:
+            return digits
+        else:
+        # if the sum is 10, set current digit to zero and repeat
+            digits[i] = 0
 
+    return [1] + digits #--> O(n)
+# Time complexity O(n)
+
+
+print("O(n) operation-->", plus_one([1,3,2])) # [1, 3, 3]
+print("O(n) operation-->", plus_one([3,2,1,9])) # [3, 2, 2, 0]
+print("O(n) operation-->", plus_one([9,9,9])) # [1, 0, 0, 0]
+
+def plus_one_josh_version(digits):
+    pass
+
+
+print("Josh's version-->", plus_one_josh_version([1,3,2]))
+print("Josh's version-->", plus_one_josh_version([3,2,1,9]))
+print("Josh's version-->", plus_one_josh_version([9,9,9]))
